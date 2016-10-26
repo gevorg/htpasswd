4d01239a93590b1455ae9cb0671b6efd59603360
Gevorg Harutyunyan <gevorg.ha@gmail.com>
2016-10-26 17:14:24 +0400

    Check for newline test.

--- test/newline.js
+++ test/newline.js
@@ -11,7 +11,9 @@ describe('newline', () => {
     // Tests for sha1.
     describe('#check', () => {
         it('line endings should be linux style', (done) => {
-            fs.readFile(__dirname + './../bin/htpasswd', (err, data) => {
+            console.log(__dirname + '/../bin/htpasswd');
+
+            fs.readFile(__dirname + '/../bin/htpasswd', (err, data) => {
                 expect(-1 === data.toString().indexOf('\n\r')).to.true;
                 done();
             });
